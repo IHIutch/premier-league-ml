cfafa88709484706a68ca6a31f932e1356e047cc
IHIutch <jbhutch01@gmail.com>
2020-12-04 23:06:49 -0500

    Add new data

--- src/App.vue
+++ src/App.vue
@@ -103,7 +103,7 @@ export default {
         filter: ""
       },
       didTrain: false,
-      weeksOfData: 8,
+      weeksOfData: 10,
       dataRange: 5,
       columns: [
         // "xP",
--- src/trainings/index.js
+++ src/trainings/index.js
@@ -1,5 +1,9 @@
 import week1 from "./trained-2020-10-29.json";
+import week2 from "./trained-2020-11-20.json";
+import week3 from "./trained-2020-12-04.json";
 
 export default {
-  week1
+  week1,
+  week2,
+  week3
 };
